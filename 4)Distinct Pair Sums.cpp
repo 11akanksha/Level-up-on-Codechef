38b04c9559885a6f6bcff1c05a4110a785958d11
11akanksha <64818557+11akanksha@users.noreply.github.com>
2022-08-19 23:05:42 +0530

    Create 4)Distinct Pair Sums.cpp

---
+++ 4)Distinct Pair Sums.cpp
@@ -0,0 +1,17 @@
+// Ques: https://www.codechef.com/submit/MANYSUMS?tab=statement
+// Code: MANYSUMS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int l,r;
+	    cin>>l>>r;
+	    
+	    cout<<(2*r - 2*l + 1)<<endl;
+	}
+	return 0;
+}
